9b47cea277bb3ab0f85fe27221ba4d82a495634e
Saleem Abdulrasool <compnerd@compnerd.org>
2021-02-13 15:08:29 -0800

    add copyright headers
    Add missing copyright headers to the Core and Runtime libraries.

--- Source/Core/OptionSet.swift
+++ Source/Core/OptionSet.swift
@@ -1,3 +1,6 @@
+// Copyright © 2018 Saleem Abdulrasool <compnerd@compnerd.org>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
 
 public protocol OptionSet : RawRepresentable {
   associatedtype Element = Self
--- Source/Core/Void.swift
+++ Source/Core/Void.swift
@@ -1,2 +1,5 @@
+// Copyright © 2018 Saleem Abdulrasool <compnerd@compnerd.org>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
 
 public typealias Void = ()
