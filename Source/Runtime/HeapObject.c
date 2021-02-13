427b449a0f489621369377f96ff5332761e60b4d
Saleem Abdulrasool <compnerd@compnerd.org>
2021-02-13 09:41:37 -0800

    Runtime: add additional stubs for building uSwift
    Ensure that we have placeholders for the referenced functions.  This
    allows us to build a version of uSwift, even if it is unable to handle
    these callouts.

---
+++ Source/Runtime/HeapObject.c
@@ -0,0 +1,8 @@
+
+#include <stdlib.h>
+
+#include "Types.h"
+#include "Visibility.h"
+
+SWIFT_RUNTIME_ABI
+HeapObject *swift_retain(HeapObject *object) { return NULL; }
---
+++ Source/Runtime/Visibility.h
@@ -0,0 +1,15 @@
+
+#ifndef uSwift_Runtime_Visibility_h
+#define uSwift_Runtime_Visibility_h
+
+#if defined(__ELF__)
+#define SWIFT_RUNTIME_ABI __attribute__((__visibility__("default")))
+#elif defined(__MACH__)
+#define SWIFT_RUNTIME_ABI __attribute__((__visibility__("default")))
+#elif defined(__WASM__)
+#define SWIFT_RUNTIME_ABI __attribute__((__visibility__("default")))
+#else
+#define SWIFT_RUNTIME_ABI __declspec(dllexport)
+#endif
+
+#endif
