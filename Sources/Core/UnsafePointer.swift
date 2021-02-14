a73144f2df2cd09a285a542d01d215aef7bc62d8
zoecarver <z.zoelec2@gmail.com>
2021-02-13 22:04:04 -0800

    Add UnsafePointer and UnsafeMutablePointer.

---
+++ Sources/Core/UnsafePointer.swift
@@ -0,0 +1,13 @@
+// Copyright © 2021 Zoe Carver <zoe@zoecarver.com>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
+
+@frozen
+public struct UnsafePointer<Pointee> {
+  public let _rawValue: Builtin.RawPointer
+
+  @_transparent
+  public init(_ _rawValue: Builtin.RawPointer) {
+    self._rawValue = _rawValue
+  }
+}
