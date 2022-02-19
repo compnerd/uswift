56530cacae2f6e59ee6cca21e98b5d8be6932aa9
Max Desiatov <max@desiatov.com>
2022-02-19 21:58:02 +0000

    Add `precondition` implementation

---
+++ Sources/Core/Assert.swift
@@ -0,0 +1,10 @@
+// Copyright © 2022 Max Desiatov <max@desiatov.com>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
+
+@_transparent
+public func precondition(_ condition: @autoclosure () -> Bool, _: StaticString,
+  file _: StaticString = #file, line _: UInt = #line) {
+  let error = !condition()
+  Builtin.condfail_message(error._value, StaticString("precondition failure").unsafeRawPointer)
+}
