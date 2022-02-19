19319d8a5df8eb1fd34b40ba91b56bce7cd965f2
Max Desiatov <max@desiatov.com>
2022-02-19 21:46:25 +0000

    Add `Builtin.swift` with `minAllocationAlignment`

---
+++ Sources/Core/Builtin.swift
@@ -0,0 +1,9 @@
+// Copyright © 2022 Max Desiatov <max@desiatov.com>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
+
+@inlinable
+@inline(__always)
+internal func minAllocationAlignment() -> Int {
+  16
+}
