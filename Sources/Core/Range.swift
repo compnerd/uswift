abe4489123cbb167d82ff063fc536cf68ccb9149
Max Desiatov <max@desiatov.com>
2022-02-19 22:22:27 +0000

    Add `Range` implementation

---
+++ Sources/Core/Range.swift
@@ -0,0 +1,33 @@
+// Copyright © 2022 Max Desiatov <max@desiatov.com>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
+
+@frozen
+public struct Range<Bound: Comparable> {
+  public let lowerBound: Bound
+
+  public let upperBound: Bound
+
+  @_alwaysEmitIntoClient
+  @inline(__always)
+  init(_uncheckedBounds bounds: (lower: Bound, upper: Bound)) {
+    lowerBound = bounds.lower
+    upperBound = bounds.upper
+  }
+
+  @inlinable
+  public init(uncheckedBounds bounds: (lower: Bound, upper: Bound)) {
+    precondition(bounds.lower <= bounds.upper, "Range requires lowerBound <= upperBound")
+    self.init(_uncheckedBounds: (lower: bounds.lower, upper: bounds.upper))
+  }
+
+  @inlinable
+  public func contains(_ element: Bound) -> Bool {
+    return lowerBound <= element && element < upperBound
+  }
+
+  @inlinable
+  public var isEmpty: Bool {
+    return lowerBound == upperBound
+  }
+}
