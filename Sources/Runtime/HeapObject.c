3c1fbca1596d51697ca7120c96ccd8b65dc23837
Max Desiatov <max@desiatov.com>
2022-02-20 19:49:52 +0000

    Runtime: fix missing return value warning

--- Sources/Runtime/HeapObject.c
+++ Sources/Runtime/HeapObject.c
@@ -11,7 +11,9 @@ SWIFT_RUNTIME_ABI
 HeapObject *swift_retain(HeapObject *object) { return NULL; }
 
 SWIFT_RUNTIME_ABI
-void *swift_slowAlloc(size_t bytes, size_t alignMask) {}
+void *swift_slowAlloc(size_t bytes, size_t alignMask) { 
+  return NULL; 
+}
 
 SWIFT_RUNTIME_ABI
 void swift_slowDealloc(void *ptr, size_t bytes, size_t alignMask) {}
