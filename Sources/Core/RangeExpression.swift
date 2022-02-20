3e8bd712b31e5570d1ce9040accc03c3e3f0de35
Max Desiatov <max@desiatov.com>
2022-02-20 17:55:54 +0000

    Add `RangeExpression` protocol

---
+++ Sources/Core/RangeExpression.swift
@@ -0,0 +1,18 @@
+// Copyright © 2022 Max Desiatov <max@desiatov.com>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
+
+public protocol RangeExpression {
+  associatedtype Bound: Comparable
+
+  func relative<C: Collection>(to collection: C) -> Range<Bound>
+      where C.Index == Bound
+  func contains(_ element: Bound) -> Bool
+}
+
+extension RangeExpression {
+  @inlinable
+  public static func ~= (_ pattern: Self, _ value: Bound) -> Bool {
+    return pattern.contains(value)
+  }
+}
