e3909ab3fd13ee048f2a69a89bc0456b7e8be961
Max Desiatov <max@desiatov.com>
2022-02-20 22:06:48 +0000

    Core: add `SignedNumeric` protocol (#56)
    * Add `SignedNumeric` protocol
    
    Co-authored-by: Saleem Abdulrasool <compnerd@compnerd.org>

---
+++ Sources/Core/SignedNumeric.swift
@@ -0,0 +1,14 @@
+// Copyright © 2022 Max Desiatov <max@desiatov.com>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
+
+public protocol SignedNumeric: Numeric {
+  static prefix func - (_ operand: Self) -> Self
+
+  mutating func negate()
+}
+
+// FIXME(MaxDesiatov): extension with default implementations of `prefix func -` and
+// `negate()` are missing. This extension currently causes assertions in the
+// type checker. We assume that this is caused by our iterative approach, which
+// temporarily excluded certain requirements from previously added protocols.
