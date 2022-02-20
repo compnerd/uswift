0ba3e1cbfcb80db22fc52de88aba0bf761112910
Saleem Abdulrasool <compnerd@compnerd.org>
2022-02-20 18:59:06 +0000

    Core: adjust indentation in `Assert.swift` (NFC)

--- Sources/Core/Assert.swift
+++ Sources/Core/Assert.swift
@@ -4,7 +4,9 @@
 
 @_transparent
 public func precondition(_ condition: @autoclosure () -> Bool, _: StaticString,
-  file _: StaticString = #file, line _: UInt = #line) {
+                         file _: StaticString = #file, line _: UInt = #line) {
   let error = !condition()
-  Builtin.condfail_message(error._value, StaticString("precondition failure").unsafeRawPointer)
+  Builtin.condfail_message(error._value,
+                           StaticString("precondition failure")
+                              .unsafeRawPointer)
 }
