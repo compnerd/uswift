d59357c1b5ba3ad19698d0ad6335f11f0e29ed1e
Max Desiatov <max@desiatov.com>
2022-02-20 20:15:01 +0000

    Core: add `BidirectionalCollection`

--- Sources/Core/BidirectionalCollection.swift
+++ Sources/Core/BidirectionalCollection.swift
@@ -3,20 +3,19 @@
 // SPDX-License-Identifier: BSD-3
 
 public protocol BidirectionalCollection: Collection
-    where SubSequence: BidirectionalCollection, Indices: BidirectionalCollection {
+    where SubSequence: BidirectionalCollection,
+    Indices: BidirectionalCollection {
   override var indices: Indices { get }
 
   override subscript(_: Range<Index>) -> SubSequence { get }
+  override func index(after i: Index) -> Index
+  override func formIndex(after i: inout Index)
 
   func index(before i: Index) -> Index
   func formIndex(before i: inout Index)
-
-  override func index(after i: Index) -> Index
-  override func formIndex(after i: inout Index)
 
   @_nonoverride func index(_ i: Index, offsetBy distance: Int) -> Index
   @_nonoverride func index(_ i: Index, offsetBy distance: Int,
                            limitedBy limit: Index) -> Index?
   @_nonoverride func distance(from start: Index, to end: Index) -> Int
-
 }
