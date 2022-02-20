e3f70ca7c90ef0c83ebec156bcc28761df23dabd
Max Desiatov <max@desiatov.com>
2022-02-20 17:50:56 +0000

    Add `Collection` protocol

---
+++ Sources/Core/Collection.swift
@@ -0,0 +1,32 @@
+// Copyright © 2022 Max Desiatov <max@desiatov.com>.
+// All Rights Reserved.
+// SPDX-License-Identifier: BSD-3
+
+public protocol Collection: Sequence {
+  associatedtype Iterator
+
+  associatedtype Index: Comparable
+  associatedtype Indices: Collection where Indices.Element == Index,
+      Indices.Index == Index, Indices.SubSequence == Indices
+
+  associatedtype SubSequence: Collection where SubSequence.Index == Index,
+      Element == SubSequence.Element, SubSequence.SubSequence == SubSequence
+
+  var startIndex: Index { get }
+  var endIndex: Index { get }
+
+  var indices: Indices { get }
+  var isEmpty: Bool { get }
+  var count: Int { get }
+
+  @_borrowed
+  subscript(_: Index) -> Element { get }
+  subscript(_: Range<Index>) -> SubSequence { get }
+
+  func index(_: Index, offsetBy distance: Int) -> Index
+  func index(_: Index, offsetBy distance: Int, limitedBy limit: Index) -> Index?
+  func distance(from start: Index, to end: Index) -> Int
+
+  func index(after: Index) -> Index
+  func formIndex(after: inout Index)
+}
